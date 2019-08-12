1e9a68e4ea8a33160dbfd86c96a2d88c4bb0b53f
yazdan <yazdan000jjja@gmail.com>
2019-08-12 13:51:13 +0430

    start project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ochsa
+
+### About
+ochsa is a changer file : ochsa change pdf files to text files
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ochsa',
+    version='0.0.1',
+    description='change pdf file to test file',
+    author='yazdan',
+    author_email='yazdan000jjja@gmail.com',
+    )
